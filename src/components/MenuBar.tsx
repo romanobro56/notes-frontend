a6b96edc20a042bc789af4ac28dc5510338e3b8b
romanobro56 <romanp2929@gmail.com>
2023-02-28 08:35:12 -0500

    added about page and (nonfunctioning) login page

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,11 +1,15 @@
 import { Component } from 'solid-js';
+import { A } from 'solid-start';
 
 const MenuBar: Component = () => {
   return (
     <div class="w-screen h-12 bg-gray-600 flex flex-row justify-around">
-      <button class="rounded-md text-gray-200 hover:bg-gray-800 h-8 w-16 mt-2">About</button>
-      <button class="rounded-md text-gray-200 hover:bg-gray-800 h-8 w-16 mt-2">Home</button>
-      <button class="rounded-md text-gray-200 hover:bg-gray-800 h-8 w-20 mt-2">Account</button>
+      <A href="/about"><button class="rounded-md text-gray-200 hover:bg-gray-800 h-8 w-16 mt-2">
+        About</button></A>
+      <A href="/"><button class="rounded-md text-gray-200 hover:bg-gray-800 h-8 w-16 mt-2">
+        Home</button></A>
+      <A href="/account"><button class="rounded-md text-gray-200 hover:bg-gray-800 h-8 w-20 mt-2">
+        Account</button></A>
     </div>
   )
 }
---
+++ src/routes/about.tsx
@@ -0,0 +1,22 @@
+ import { Component } from 'solid-js';
+import MenuBar from '~/components/MenuBar';
+ 
+ const about: Component = () => {
+  return (
+    <div>
+      <MenuBar />
+      <p class="text-center font-mono text-5xl mt-6">notes app</p>
+      <p class="text-center font-thin text-xl mt-4">yeah, that's about it.</p>
+      <div class="flex flex-col w-full items-center">
+        <div class="bg-slate-400 rounded-lg w-100 flex flex-col items-center mt-6">
+          <p class="text-center font-normal text-2xl mt-2 mx-4">check out my Github profile here:</p>
+          <a href="https://github.com/romanobro56">
+            <img src="/minion-money.png" alt="profile here" class="rounded-full h-48 border-4 border-slate-800 my-4" />
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+ }
+ 
+ export default about;
