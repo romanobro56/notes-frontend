4951079763d29db9c310eeaa85e1cf5865b55f1c
romanobro56 <romanp2929@gmail.com>
2023-03-10 20:50:33 -0500

    netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+import netlify from "solid-start-netlify";
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
-
 export default defineConfig({
-  plugins: [solid({ ssr: false })],
-  optimizeDeps: {exclude: ["@solidjs/router"]}
-  //the solid js router does not work in development mode in vite without the router being excluded
+  plugins: [
+    solid({
+      adapter: netlify({ edge: false })
+    })
+  ]
 });
